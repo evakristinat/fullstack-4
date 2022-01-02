345a17b6abd6b00dd684a6de7868149d39929a03
Eeva Takanen <eevakristiinatakanen@gmail.com>
2022-01-02 18:50:59 +0200

    4.3.

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,5 +1,11 @@
+const { error } = require('./logger')
+
 const dummy = (blogs) => {
-  return 1
+  if (Array.isArray(blogs)) {
+    return 1
+  } else {
+    error('the parameter needs to be an array')
+  }
 }
 
 module.exports = { dummy }
