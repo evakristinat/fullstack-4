d7a1fdbde5b74dcc3d38de82f173032921dbd761
Eeva Takanen <eevakristiinatakanen@gmail.com>
2022-01-03 11:38:36 +0200

    comments

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -4,6 +4,7 @@ const dummy = (blogs) => {
   return Array.isArray(blogs) ? 1 : error('the parameter needs to be an array')
 }
 
+//Lasketaan yhteen kaikki tykkäykset koko listasta
 const totalLikes = (blogs) => {
   return Array.isArray(blogs)
     ? blogs.reduce((sum, item) => {
@@ -12,7 +13,8 @@ const totalLikes = (blogs) => {
     : error('the parameter needs to be an array')
 }
 
-//Blogit sortataan kasvavassa järjestyksessä ja viimeinen, eli suurin, palautetaan.
+/*Etsitään eniten tykkäuksiä saanut blogi listasta. Blogit sortataan kasvavassa järjestyksessä ja
+  viimeinen, eli suurin, palautetaan siistityssä formaatissa.*/
 const favoriteBlog = (blogs) => {
   if (Array.isArray(blogs)) {
     const blogsToSort = [...blogs]
@@ -28,28 +30,38 @@ const favoriteBlog = (blogs) => {
   }
 }
 
+/*Blogeista etsitään eniten esiintyvä kirjoittaja ja palautetaan se siistityssä formaatissa*/
 const mostBlogs = (blogs) => {
   if (Array.isArray(blogs) && blogs.length > 1) {
+    //pelkistetään blogilista pelkiksi kirjoittajiksi
     const authors = blogs.map((blog) => blog.author)
+    //kootaan uniikit kirjoittajat yhteen
+    const uniqueAuthors = [...new Set(authors)]
+    let counted = []
+
+    /*Lasketaan halutun kirjailijan esiintyminen annetussa taulukossa.
+    Jos ja kun listassa kohdataan haluttu nimi, lisätään esiintymisien määrää
+    yhdellä, muuten määrä pysyy samana.*/
     const countAuthors = (allAuthors, searchedAuthor) =>
       allAuthors.reduce((amount, author) => {
         return author === searchedAuthor ? amount + 1 : amount
       }, 0)
 
-    let all = []
-
-    const uniqueAuthors = [...new Set(authors)]
-
+    /*Käydään läpi lista blogien kirjoittajista kerran kutakin uniikkia kirjoittajaa kohden
+    ja lisätään tulokset yhteen counted-taulukkoon.*/
     for (const author of uniqueAuthors) {
       const amount = countAuthors(authors, author)
-      all.push({
+      counted.push({
         author: author,
         blogs: amount,
       })
     }
 
-    const sorted = all.sort((a, b) => a.blogs - b.blogs)
+    //järjestetään lasketut tulokset kasvavaan järjestykseen ja palautetaan viimeinen (suurin)
+    const sorted = counted.sort((a, b) => a.blogs - b.blogs)
     return sorted.pop()
+
+    //jos parametriksi annetaan taulukko, jossa on vain yksi blogi, palautetaan se oikeassa formaatissa
   } else if (blogs.length === 1) {
     return {
       author: blogs[0].author,
@@ -60,28 +72,37 @@ const mostBlogs = (blogs) => {
   }
 }
 
+/*Blogeista etsitään eniten tykkäyksiä saanut kirjoittaja ja palautetaan se siistityssä formaatissa*/
 const mostLikes = (blogs) => {
   if (Array.isArray(blogs) && blogs.length > 1) {
+    //pelkistetään blogilista pelkiksi kirjoittajiksi
     const authors = blogs.map((blog) => blog.author)
-    let all = []
+    //kootaan uniikit kirjoittajat yhteen
+    const uniqueAuthors = [...new Set(authors)]
+    let counted = []
 
+    /*Lasketaan yhteen annetun kirjoittajan tykkäykset annetussa listassa.
+    Aina kun haluttu kirjoittaja tulee listassa vastaan, lisätään tykkäyksiä ko. blogin tykkäyksien verran*/
     const countLikes = (allBlogs, searchedAuthor) =>
       allBlogs.reduce((likes, blog) => {
         return blog.author === searchedAuthor ? likes + blog.likes : likes
       }, 0)
 
-    const uniqueAuthors = [...new Set(authors)]
-
+    /*Käydään läpi lista blogien kirjoittajista kerran kutakin uniikkia kirjoittajaa kohden
+    ja lisätään tulokset yhteen counted-taulukkoon.*/
     for (const author of uniqueAuthors) {
       const likes = countLikes(blogs, author)
-      all.push({
+      counted.push({
         author: author,
         likes: likes,
       })
     }
 
-    const sorted = all.sort((a, b) => a.likes - b.likes)
+    //järjestetään lasketut tulokset kasvavaan järjestykseen ja palautetaan viimeinen (suurin)
+    const sorted = counted.sort((a, b) => a.likes - b.likes)
     return sorted.pop()
+
+    //jos parametriksi annetaan taulukko, jossa on vain yksi blogi, palautetaan se oikeassa formaatissa
   } else if (blogs.length === 1) {
     return {
       author: blogs[0].author,
