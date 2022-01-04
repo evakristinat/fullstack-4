02cc72f81b8a8784c709862876fa29ce1af78676
Eeva Takanen <eevakristiinatakanen@gmail.com>
2022-01-04 18:41:22 +0200

    4.18.

--- utils/middleware.js
+++ utils/middleware.js
@@ -18,6 +18,12 @@ const errorHandler = (error, req, res, next) => {
     return res.status(400).send({ error: error.message })
   } else if (error.name === 'TypeError') {
     return res.status(400).send({ error: error.message })
+  } else if (error.name === 'JsonWebTokenError') {
+    return res.status(401).json({ error: 'invalid token' })
+  } else if (error.name === 'TokenExpiredError') {
+    return res.status(401).json({
+      error: 'token expired',
+    })
   }
 
   next(error)
